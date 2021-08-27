1c8c6af8f74ee39ed3892d9067ae1867f5a7539d
Joseph Sullivan <josephsullivan256@gmail.com>
2021-08-27 12:33:51 -0700

    further subdivided lu() into other methods

--- src/lib/linalg/lu.rs
+++ src/lib/linalg/lu.rs
@@ -3,82 +3,58 @@ use super::mat::Mat;
 use super::util::get_max_index;
 
 
+pub struct RREFDecomposition<F: Field> {
+	pub p : Mat<F>,
+	pub rref : Mat<F>
+}
+
 pub struct LUDecomposition<F: Field> {
 	pub p : Mat<F>,
 	pub l : Mat<F>,
 	pub u : Mat<F>
 }
 
-pub struct RREFDecomposition<F: Field> {
-	pub p : Mat<F>,
-	pub rref : Mat<F>
+struct LUDecompositionInternal<F: Field> {
+	pub p: Vec<usize>,
+	pub l: Mat<F>,
+	pub u: Mat<F>
 }
 
+impl<F: Field> LUDecompositionInternal<F> {
 
-impl<F: Field+StabilityCmp+EpsilonEquality> Mat<F> {
+	// assumes src < target
+	fn permute(&mut self, src: usize, target: usize) {
+		self.p.swap(src, target);
+		self.u.permute(src, target);
+		self.l.permute_under_diagonal(src, target);
+	}
 
-	/// Returns LU Decomposition of given matrix. This method does not make
-	/// any assumptions on the dimension of the matrix (i.e. we allow
-	/// rectangular matrices), though it does require the field to implement 
-	/// StabilityCmp in order to do Gaussian elimination under the hood.
-	/// 
-	/// The LU decomposition satisfies more requirements than that typically
-	/// seen in Fortran implementations. The `u` matrix is actually in row
-	/// echelon form, which will be convenient for calculating rank as well as
-	/// rref (which will be used to compute a basis for the kernel)
-	pub fn lu(&self) -> LUDecomposition<F> {
-		// Matrix that is eventually made into row echelon form. It
-		// will become upper triangular.
-		let mut u = self.clone();
-		let n = u.rows();
+	fn scale_row(&mut self, r: usize, scalar: F) {
+		self.l[(r,r)] = scalar;
+		self.u.scale(r, F::ONE/scalar);
+	}
 
-		// elem_prod is product of inverses of elementary matrices applied
-		// to `mat`, perm is permutation, such that
-		// `perm * self=elem_prod * mat`
-		// or equivalently
-		// `self = perm^T * elem_prod * mat`
-		let mut p_vec : Vec<usize> = (0..n).collect();
-		let mut l : Mat<F> = Mat::identity(n);
-
-		// row that hasn't been cleared yet
-		let mut r = 0;
-
-		// iterate through all columns
-		for c in 0..u.cols() {
-			// if cleared out all the rows before we've gone through the
-			// columns, just finish here
-			if r >= u.rows() {
-				break;
-			}
-
-			if let Some((stable_index, scalar)) = u.get_stable_column_index_under_row(c, r) {
-				// permute
-				p_vec.swap(r, stable_index);
-				u.permute(r, stable_index);
-				l.permute_under_diagonal(r, stable_index);
-
-				// scale l and u correspondingly
-				l[(r,r)] = scalar;
-				u.scale(r, F::ONE/scalar);
-
-				// then, we do replacement
-				for r2 in (r+1)..u.rows() {
-					let scalar2 = u[(r2,c)];
-					u.replace(r, r2, F::ZERO-scalar2);
-					l[(r2, r)] = scalar2;
-				}
-				
-				// go on to the next row, since after doing replacement we should
-				// only look beyond in order to get row echelon form.
-				r += 1;
-			}
-		}
-		LUDecomposition{
-			p: Mat::permutation_from_vec(p_vec),
-			l: l,
-			u: u
+	fn replace_col_under_row(&mut self, r: usize, c: usize) {
+		for r2 in (r+1)..self.u.rows() {
+			let scalar2 = self.u[(r2,c)];
+			self.u.replace(r, r2, F::ZERO-scalar2);
+			self.l[(r2, r)] = scalar2;
 		}
 	}
+}
+
+impl<F: Field> Mat<F> {
+	// assumes src < target
+	fn permute_under_diagonal(&mut self, src: usize, target: usize) {
+		for c in 0..src {
+			let temp = self[(src, c)];
+			self[(src, c)] = self[(target, c)];
+			self[(target, c)] = temp;
+		}
+	}
+}
+
+impl<F: Field+StabilityCmp+EpsilonEquality> Mat<F> {
 
 	fn get_stable_column_index_under_row(&self, c: usize, r: usize)-> Option<(usize, F)>
 	{
@@ -95,12 +71,51 @@ impl<F: Field+StabilityCmp+EpsilonEquality> Mat<F> {
 		}
 	}
 
-	// assumes src < target
-	fn permute_under_diagonal(&mut self, src: usize, target: usize) {
-		for c in 0..src {
-			let temp = self[(src, c)];
-			self[(src, c)] = self[(target, c)];
-			self[(target, c)] = temp;
+	/// Returns LU Decomposition of given matrix. This method does not make
+	/// any assumptions on the dimension of the matrix (i.e. we allow
+	/// rectangular matrices), though it does require the field to implement 
+	/// StabilityCmp in order to do Gaussian elimination under the hood.
+	/// 
+	/// The LU decomposition satisfies more requirements than that typically
+	/// seen in Fortran implementations. The `u` matrix is actually in row
+	/// echelon form, which will be convenient for calculating rank as well as
+	/// rref (which will be used to compute a basis for the kernel)
+	/// 
+	/// `l` is product of inverses of elementary matrices applied to `mat`,
+	/// `p` is permutation, such that `(p * self).epsilon_equals(l * u)`
+	/// or equivalently `self.epsilon_equals(p.transpose() * l * u)`.
+	pub fn lu(&self) -> LUDecomposition<F> {
+		let m = self.cols();
+		let n = self.rows();
+
+		// the "in progress" LU decomposition that will eventually become what
+		// we want.
+		let mut lu = LUDecompositionInternal {
+			p: (0..n).collect(),
+			l: Mat::identity(n),
+			u: self.clone()
+		};
+
+		// row that hasn't been cleared yet
+		let mut r = 0;
+
+		for c in 0..m {
+			if r >= n {
+				break;
+			}
+
+			if let Some((stable_index, scalar)) = lu.u.get_stable_column_index_under_row(c, r) {
+				lu.permute(r, stable_index);
+				lu.scale_row(r, scalar);
+				lu.replace_col_under_row(r, c);
+				
+				r += 1;
+			}
+		}
+		LUDecomposition{
+			p: Mat::permutation_from_vec(lu.p),
+			l: lu.l,
+			u: lu.u
 		}
 	}
 
@@ -133,38 +148,6 @@ mod tests {
 	use super::Mat;
 	use super::super::super::num::EpsilonEquality;
 	use super::super::util::num_to_seq;
-	
-	// #[test]
-	// fn test_lu_1(){
-	// 	let mat : Mat<f32> = Mat::new(5,5,
-	// 		vec![
-	// 			5.0, 5.0, 5.0,  5.0,  1.0,
-	// 			4.0, 5.0, 6.0,  7.0,  1.0,
-	// 			8.0, 9.0, 10.0, 11.0, 1.0,
-	// 			0.0, 9.0, 2.0,  3.0,  1.0,
-	// 			7.0, 6.0, 9.0,  4.0,  1.0
-	// 		]
-	// 	);
-	// 	let lu = mat.lu();
-
-	// 	assert_eq!(mat, &lu.p.transpose() * &(&lu.l * &lu.u));
-	// }
-
-	#[test]
-	fn bad_case() {
-		let mut mat : Mat<f64> = Mat::new(3, 4,
-			vec![
-				1.0, 2.0, 2.0, 0.0,
-				2.0, 0.0, 1.0, 0.0,
-				2.0, 1.0, 0.0, 0.0
-			]
-		);
-		let lu = mat.lu();
-		println!("{}", lu.u);
-		println!("{}", lu.l);
-		println!("{}", lu.p);
-		assert!(mat.epsilon_equals(&(&lu.p.transpose() * &(&lu.l * &lu.u))));
-	}
 
 	#[test]
 	fn test_lu() {
